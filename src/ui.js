84ad012761f33d59591c98e81aaceaac541e6cfe
Andrew Wei <andrewscwei@gmail.com>
2014-11-13 23:03:59 -0800

    Added transform2D() and translate2D()

--- src/ui.js
+++ src/ui.js
@@ -10,16 +10,71 @@
 define
 (
     [
+        'utils',
         'ui/viewcontroller'
     ],
-    function(viewcontroller)
+    function(utils, ViewController)
     {
         var api = function(obj)
         {
             return obj;
         };
 
-        api.ViewController = viewcontroller;
+        api.ViewController = ViewController;
+
+        /**
+         * Translates a DOM element in 2D.
+         * @param  {object} element    Target DOM element
+         * @param  {object} properties Translation properties: top/right/bottom/left
+         *                             (if this argument is unspecified, all translation styles
+         *                             will be reset to 'initial')
+         */
+        function translate2D(element, properties)
+        {
+            utils.assert(element && element.style, 'Invalid element in argument.');
+
+            if (!element || !element.style) return;
+
+            if (properties)
+            {
+                if (properties.top) element.style.top = properties.top;
+                if (properties.right) element.style.right = properties.right;
+                if (properties.bottom) element.style.bottom = properties.bottom;
+                if (properties.left) element.style.left = properties.left;
+            }
+            else
+            {
+                element.style.top = 'initial';
+                element.style.right = 'initial';
+                element.style.bottom = 'initial';
+                element.style.left = 'initial';
+            }
+        } api.translate2D = translate2D;
+
+        /**
+         * Transforms a DOM element in 2D.
+         * @param  {object} element    Target DOM element.
+         * @param  {object} properties Transformation properties: width/height
+         *                             (if this argument is unspecified, all transformation styles
+         *                             will be reset to 'initial')
+         */
+        function transform2D(element, properties)
+        {
+            utils.assert(element && element.style, 'Invalid element in argument.');
+
+            if (!element || !element.style) return;
+
+            if (properties)
+            {
+                if (properties.width) element.style.width = properties.width;
+                if (properties.height) element.style.height = properties.height;
+            }
+            else
+            {
+                element.style.width = 'initial';
+                element.style.height = 'initial';
+            }
+        } api.transform2D = transform2D;
 
         return api;
     }
