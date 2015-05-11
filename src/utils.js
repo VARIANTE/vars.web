d44602129e440c7ee59bebaedd5f9a81a1c97b92
Andrew Wei <andrew@variante.io>
2015-05-11 01:00:46 -0700

    Add utils.module() to quickly instantiate modules when DOM is ready

--- src/utils.js
+++ src/utils.js
@@ -16,6 +16,7 @@ define
         'utils/namespace',
         'utils/inherit',
         'utils/sizeOf',
+        'utils/module',
         'utils/keyOfValue',
         'utils/isNull',
         'utils/AssetLoader'
@@ -28,6 +29,7 @@ define
         namespace,
         inherit,
         sizeOf,
+        module,
         keyOfValue,
         isNull,
         AssetLoader
@@ -41,6 +43,7 @@ define
         Object.defineProperty(api, 'namespace', { value: namespace, writable: false, enumerable: true });
         Object.defineProperty(api, 'inherit', { value: inherit, writable: false, enumerable: true });
         Object.defineProperty(api, 'sizeOf', { value: sizeOf, writable: false, enumerable: true });
+        Object.defineProperty(api, 'module', { value: module, writable: false, enumerable: true });
         Object.defineProperty(api, 'keyOfValue', { value: keyOfValue, writable: false, enumerable: true });
         Object.defineProperty(api, 'isNull', { value: isNull, writable: false, enumerable: true });
         Object.defineProperty(api, 'AssetLoader', { value: AssetLoader, writable: false, enumerable: true });
---
+++ src/utils/module.js
@@ -0,0 +1,68 @@
+/**
+ *  vars.js
+ *  (c) VARIANTE (http://variante.io)
+ *
+ *  This software is released under the MIT License:
+ *  http://www.opensource.org/licenses/mit-license.php
+ */
+define
+(
+    [
+    ],
+    function
+    (
+    )
+    {
+        /**
+         * Creates a new module and attaches it to the window when DOM is ready. Option
+         * to pass an init object to initialize the module. A typical use-case will be to
+         * create a new Element module.
+         *
+         * @param  {Function}   impl Module implementation.
+         * @param  {Object}     init Optional object passed into the impl.
+         */
+        function module(impl, init)
+        {
+            if (!document) return;
+
+            var onLoaded = function(event)
+            {
+                if (document.addEventListener)
+                {
+                    document.removeEventListener('DOMContentLoaded', onLoaded, false);
+                    window.removeEventListener('load', onLoaded, false);
+                }
+                else if (document.attachEvent)
+                {
+                    document.detachEvent('onreadystatechange', onLoaded);
+                    window.detachEvent('onload', onLoaded);
+                }
+
+                var module = new impl(init);
+
+                if (window && !window.module)
+                {
+                    window.module = module;
+                }
+            };
+
+            if (document.readyState === 'complete')
+            {
+                return setTimeout(onLoaded, 1);
+            }
+
+            if (document.addEventListener)
+            {
+                document.addEventListener('DOMContentLoaded', onLoaded, false);
+                window.addEventListener('load', onLoaded, false);
+            }
+            else if (document.attachEvent)
+            {
+                document.attachEvent('onreadystatechange', onLoaded);
+                window.attachEvent('onload', onLoaded);
+            }
+        }
+
+        return module;
+    }
+);
