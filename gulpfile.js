e3a856341569948d9625258b8830350075b599d2
Andrew Wei <andrew@variante.io>
2015-05-30 16:48:54 -0700

    Add Sass library

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,14 @@
  *  http://www.opensource.org/licenses/mit-license.php
  */
 
+var SCRIPTS_PATTERN = 'js';
+var STYLES_PATTERN = '{css,scss}';
+var SOURCEMAPS_PATTERN = '{css.map,js.map}';
+
 var gulp = require('gulp');
+var merge = require('merge-stream');
 var r = require('requirejs');
+var $ = require('gulp-load-plugins')();
 
 /**
  * Clean task.
@@ -19,23 +25,72 @@ var r = require('requirejs');
 gulp.task('clean', require('del').bind(null, ['dist']));
 
 /**
- * Build bask.
+ * Wire CSS/JavaScript dependencies.
  */
-gulp.task('build', ['clean'], function()
+gulp.task('wiredep', function()
+{
+    return merge
+    (
+        // CSS dependencies.
+        gulp.src('node_modules/normalize.css/normalize.css')
+            .pipe($.rename('normalize.scss'))
+            .pipe(gulp.dest('src/sass'))
+    );
+});
+
+/**
+ * Builds the Sass library.
+ */
+gulp.task('styles', ['wiredep'], function()
+{
+    return merge
+    (
+        // Raw
+        gulp.src('src/sass/vars.'+STYLES_PATTERN)
+            .pipe($.sourcemaps.init())
+            .pipe($.sass({
+                outputStyle: 'nested',
+                precision: 10,
+                includePaths: ['.']
+            }))
+            .pipe($.postcss([require('autoprefixer-core')({ browsers: ['last 2 version', 'ie 9'] })]))
+            .pipe($.sourcemaps.write('./'))
+            .pipe(gulp.dest('dist/css')),
+        // Minified
+        gulp.src('src/sass/vars.'+STYLES_PATTERN)
+            .pipe($.sass({
+                outputStyle: 'nested',
+                precision: 10,
+                includePaths: ['.']
+            }))
+            .pipe($.postcss([require('autoprefixer-core')({ browsers: ['last 2 version', 'ie 9'] })]))
+            .pipe($.csso())
+            .pipe($.rename('vars.min.css'))
+            .pipe(gulp.dest('dist/css')),
+        // Copy
+        gulp.src('src/sass/**/*'+STYLES_PATTERN)
+            .pipe(gulp.dest('dist/sass'))
+    );
+});
+
+/**
+ * Builds the JavaScript library.
+ */
+gulp.task('scripts', ['wiredep'], function()
 {
     r.optimize
     (
         {
             // All paths used by the r.js optimizer will be relative to this base URL.
-            baseUrl: 'src',
+            baseUrl: 'src/js',
 
             // File of the distributed library.
-            out: 'dist/vars.js',
+            out: 'dist/js/vars.js',
 
             // Paths of modules.
             paths:
             {
-                almond: '../node_modules/almond/almond'
+                almond: '../../node_modules/almond/almond'
             },
 
             // Modules included in the distributed library.
@@ -44,8 +99,8 @@ gulp.task('build', ['clean'], function()
             // Wrapper for AMD, CommonJS and browser compatibility.
             wrap:
             {
-                startFile: 'src/_start.js',
-                endFile:   'src/_end.js'
+                startFile: 'src/js/_start.js',
+                endFile:   'src/js/_end.js'
             },
 
             // Option to minify JS files.
@@ -63,15 +118,15 @@ gulp.task('build', ['clean'], function()
     (
         {
             // All paths used by the r.js optimizer will be relative to this base URL.
-            baseUrl: 'src',
+            baseUrl: 'src/js',
 
             // File of the distributed library.
-            out: 'dist/vars.min.js',
+            out: 'dist/js/vars.min.js',
 
             // Paths of modules.
             paths:
             {
-                almond: '../node_modules/almond/almond'
+                almond: '../../node_modules/almond/almond'
             },
 
             // Modules included in the distributed library.
@@ -80,8 +135,8 @@ gulp.task('build', ['clean'], function()
             // Wrapper for AMD, CommonJS and browser compatibility.
             wrap:
             {
-                startFile: 'src/_start.js',
-                endFile:   'src/_end.js'
+                startFile: 'src/js/_start.js',
+                endFile:   'src/js/_end.js'
             },
 
             // Option to minify JS files.
@@ -97,10 +152,24 @@ gulp.task('build', ['clean'], function()
 });
 
 /**
- * Default task.
+ * Build bask.
  */
-gulp.task('default', function()
+gulp.task('build', ['styles', 'scripts']);
+
+/**
+ * Default task.
+ *
+ * @param {Boolean} --watch Specifies whether to watch for changes after the task is done.
+ */
+gulp.task('default', ['clean'], function()
 {
+    var watch = $.util.env['watch'] || $.util.env['w'];
+
     gulp.start('build');
-    gulp.watch('src/**/*.js', ['build']);
+
+    if (watch)
+    {
+        gulp.watch('src/**/*.'+STYLES_PATTERN, ['build']);
+        gulp.watch('src/**/*.'+SCRIPTS_PATTERN, ['build']);
+    }
 });
---
+++ test/test.js
@@ -0,0 +1,56 @@
+/**
+ *  vars.js
+ *  (c) VARIANTE (http://variante.io)
+ *
+ *  This software is released under the MIT License:
+ *  http://www.opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * @class
+ * Controller of the main module.
+ */
+vars.module((function() {
+
+'use strict';
+
+vars.debug = true;
+
+console.log(vars.math.clamp);
+
+/**
+ * @constructor
+ * Creates a new Test instance.
+ */
+function Test(element)
+{
+    vars.Element.call(this, element);
+} var parent = vars.inherit(Test, vars.Element);
+
+/**
+ * @inheritDoc
+ */
+Test.prototype.init = function()
+{
+    this.responsive = true;
+    this.updateDelegate.refreshRate = 20.0;
+
+    parent.prototype.init.call(this);
+};
+
+/**
+ * @inheritDoc
+ */
+Test.prototype.update = function(dirtyTypes)
+{
+    if (this.isDirty(vars.DirtyType.POSITION))
+    {
+        var element = $('#foo');
+        vars.translate(element, { top: 0 });
+        vars.translate3d(element, { x: 50, y: 50 }, { x: 25 });
+    }
+
+    parent.prototype.update.call(this);
+};
+
+return Test; }()));
