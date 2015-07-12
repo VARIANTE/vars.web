f64520fd4320f8ccad5b1a72e03739af9ad53f4c
andrewscwei <andrewscwei@gmail.com>
2015-07-11 20:25:46 -0700

    Update compiled library

--- dist/js/vars.js
+++ dist/js/vars.js
@@ -1149,7 +1149,7 @@ define
             }
             else
             {
-                return s[0];
+                return s[0].replace('state-', '');
             }
         }
 
--- test/test.js
+++ test/test.js
@@ -14,7 +14,7 @@ vars.module((function() {
 
 'use strict';
 
-vars.debug = true;
+vars.debug = false;
 
 console.log(vars.math.clamp);
 
@@ -22,9 +22,9 @@ console.log(vars.math.clamp);
  * @constructor
  * Creates a new Test instance.
  */
-function Test(element)
+function Test(init)
 {
-    vars.Element.call(this, element);
+    vars.Element.call(this, init || $('#foo').get(0));
 } var parent = vars.inherit(Test, vars.Element);
 
 /**
@@ -32,7 +32,7 @@ function Test(element)
  */
 Test.prototype.init = function()
 {
-    this.responsive = true;
+    this.updateDelegate.responsive = true;
     this.updateDelegate.refreshRate = 20.0;
 
     parent.prototype.init.call(this);
@@ -43,11 +43,10 @@ Test.prototype.init = function()
  */
 Test.prototype.update = function(dirtyTypes)
 {
-    if (this.isDirty(vars.DirtyType.POSITION))
+    if (this.updateDelegate.isDirty(vars.DirtyType.POSITION))
     {
-        var element = $('#foo');
-        vars.translate(element, { top: 0 });
-        vars.translate3d(element, { x: 50, y: 50 }, { x: 25 });
+        vars.translate(this.element, { top: 0 });
+        vars.translate3d(this.element, { x: 50, y: 50 }, { x: 25 });
     }
 
     parent.prototype.update.call(this);
